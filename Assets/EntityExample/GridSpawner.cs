e29315707743ffc52db943c98d556ac6e57ed897
JVince <vincewang10a7@gmail.com>
2023-12-14 18:21:49 +0900

    feature: Upgrate unity to newest version

--- Assets/EntityExample/GridSpawner.cs
+++ Assets/EntityExample/GridSpawner.cs
@@ -52,34 +52,34 @@ public class GridSpawner : MonoBehaviour
 
     public void BatchSpawn()
     {
-        ClearSpawns();
-
-        if (m_prefabs.Length <= 0) {
-            m_statusText.text = "No entities found with SpawnTag.";
-            return;
-        }
-
-        m_pos = transform.position;
-        m_spawnCount = int.Parse(m_inputField.text);
-        m_width = math.sqrt(m_spawnCount) * m_spacing;
-        int wholePart = m_spawnCount / m_prefabs.Length;
-        int remainder = m_spawnCount - (wholePart * m_prefabs.Length);
-        // for every prefab, spawn wholePart instances
-        for (int i = 0; i < m_prefabs.Length; i++) {
-            // if we're on the last index, include the remainder
-            if (i == m_prefabs.Length - 1) { wholePart += remainder; }
-            // do a batch instantiation
-            NativeArray<Entity> entities = m_em.Instantiate(m_prefabs[i], wholePart, Allocator.Temp);
-            // set the position for each of the new entities
-            for (int j = 0; j < entities.Length; j++) {
-                m_em.AddComponentData<Translation>(entities[j], new Translation { Value = m_pos });
-                m_em.AddComponent<SpawnedTag>(entities[j]);
-                IncrementPosition();
-            }
-            m_currentCount += entities.Length;
-            entities.Dispose();
-        }
-        UpdateCountText();
+        // ClearSpawns();
+        //
+        // if (m_prefabs.Length <= 0) {
+        //     m_statusText.text = "No entities found with SpawnTag.";
+        //     return;
+        // }
+        //
+        // m_pos = transform.position;
+        // m_spawnCount = int.Parse(m_inputField.text);
+        // m_width = math.sqrt(m_spawnCount) * m_spacing;
+        // int wholePart = m_spawnCount / m_prefabs.Length;
+        // int remainder = m_spawnCount - (wholePart * m_prefabs.Length);
+        // // for every prefab, spawn wholePart instances
+        // for (int i = 0; i < m_prefabs.Length; i++) {
+        //     // if we're on the last index, include the remainder
+        //     if (i == m_prefabs.Length - 1) { wholePart += remainder; }
+        //     // do a batch instantiation
+        //     NativeArray<Entity> entities = m_em.Instantiate(m_prefabs[i], wholePart, Allocator.Temp);
+        //     // set the position for each of the new entities
+        //     for (int j = 0; j < entities.Length; j++) {
+        //         m_em.AddComponentData<Translation>(entities[j], new Translation { Value = m_pos });
+        //         m_em.AddComponent<SpawnedTag>(entities[j]);
+        //         IncrementPosition();
+        //     }
+        //     m_currentCount += entities.Length;
+        //     entities.Dispose();
+        // }
+        // UpdateCountText();
     }
 
     void UpdateCountText()
--- Assets/EntityExample/SpawnTag.cs
+++ Assets/EntityExample/SpawnTag.cs
@@ -1,10 +1,11 @@
 // tags for spawning things
 //-------------------------------------------//
 
+using Unity.Burst;
 using Unity.Entities;
 
 // any entities found with this tag will be spawned
-[GenerateAuthoringComponent]
+[BurstCompile]
 public struct SpawnTag : IComponentData { }
 
 // any entity that is spawned will be given this tag
